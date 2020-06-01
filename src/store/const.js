60c12a4fe6cef31bb653ca04f1c9c801e9c68d71
Elvin Peng <elvinpeng@tencent.com>
2020-06-01 22:52:39 +0800

    refactor: delete storage folder

--- src/store/const.js
+++ src/store/const.js
@@ -1,4 +1,4 @@
-export const STORAGE_KEY = 'STORAGE_KEY'
+export const STORAGE_KEY = 'MEMO_STORAGE'
 export const ADD_MEMO = 'ADD_MEMO'
 export const DELETE_MEMO = 'DELETE_MEMO'
 export const EDIT_MEMO = 'EDIT_MEMO'
