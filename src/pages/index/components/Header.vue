2fe4be09365df9227fa9021040eae7ab534990ba
Elvin Peng <elvin.pjw@gmail.com>
2020-07-03 12:17:50 +0800

    fix(login): 已登陆状态判断 (#12)

--- src/pages/index/components/Header.vue
+++ src/pages/index/components/Header.vue
@@ -57,7 +57,7 @@ export default {
         return;
       }
 
-      this.hasLogin = loginState.hasLogin
+      this.hasLogin = loginState.isCustomAuth
 
       const userInfo = await cloudBase.getUserInfo()
       this.nickName = userInfo.nickName
